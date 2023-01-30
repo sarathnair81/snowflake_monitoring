52c19739611cfed5423c6a21050037198c18e2d6
sarathnair81 <118773093+sarathnair81@users.noreply.github.com>
2023-01-30 13:15:47 -0800

    Update Snowflake_Dev_Final.py

--- Snowflake_Dev_Final.py
+++ Snowflake_Dev_Final.py
@@ -5,11 +5,13 @@ from snowflake.connector import connect
 import logging 
 import json 
 import pandas as pd
+
 USER = "SVC_SNOWDEV_EDPDEV_APICA"
 PASSWORD = "sn0wflak3_SF@apicadev9"
 ACCOUNT = "deltadentalins-dev"
 DB = "edpdev"
 WAREHOUSE = "wh_edpdev_rpt"
+
 conn = connect(
     user=USER,
     password=PASSWORD,
@@ -20,27 +22,37 @@ conn = connect(
         'QUERY_TAG': 'APICA',
     }
     )
+
+
 sql1 = "SELECT CURRENT_TIMESTAMP() AS EVENT_TIMESTAMP, CURRENT_USER AS USER_NAME"
+
 sql2 = "select * from table(information_schema.login_history_by_user(user_name => 'SVC_SNOWDEV_EDPDEV_APICA', result_limit => 1)) order by event_timestamp"
+
 cursor = conn.cursor()
 cursor.execute(sql2)
 #first_row = cur.fetchone()
 df = cursor.fetch_pandas_all()
 #print(df)
 #print(first_row)
+
+
 return_code = 0
 current_time = str(df.EVENT_TIMESTAMP[0])
 current_user = df.USER_NAME[0]
 is_success = df.IS_SUCCESS[0]
 message = ("IS_LOGIN_SUCCESSFUL: " + is_success ) 
 value = df.ERROR_CODE[0]
+
+
 if value is None:
     value_code = 200
 else:
     value_code = value
+    
 #json = df.to_json()
 #print(json)
-json_return = 
+
+json_return = {
     "returncode": return_code,
     "start_time": current_time,
     "end_time": current_time,
